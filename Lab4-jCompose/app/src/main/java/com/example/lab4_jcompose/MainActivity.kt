84b2237e2bf4890ea29bbfee63a850d6e10a608d
EnyaGalvez <galvezenya@gmail.com>
2025-08-13 15:32:59 -0600

    box

--- Lab4-jCompose/app/src/main/java/com/example/lab4_jcompose/MainActivity.kt
+++ Lab4-jCompose/app/src/main/java/com/example/lab4_jcompose/MainActivity.kt
@@ -4,15 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lab4_jcompose.ui.theme.Lab4jComposeTheme
+import androidx.compose.ui.unit.dp
 
+// Main
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,8 +27,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Lab4jComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Portada(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -30,18 +36,26 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+// Composables
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun Portada(
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .border(
+                width = 6.dp,
+                color = Color(18, 110, 34)
+            )
     )
 }
 
+// Preview
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun PortadaPreview() {
     Lab4jComposeTheme {
-        Greeting("Android")
+        Portada(modifier = Modifier.fillMaxSize())
     }
 }
